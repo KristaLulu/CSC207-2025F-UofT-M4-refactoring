a8fab88ccd4c2b547be691ae126b585ab2602df9
Xiaotong Zhu <xiaotong.zhu@mail.utoronto.ca>
2025-11-13 03:02:19 -0500

    Task 1.1: fix all CheckStyle issues

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,37 @@
 package theater;
 
+/**
+ * Class representing a play.
+ */
+
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
+    /**
+     * Constructs a new Play with the given name and type.
+     * @param name the name of the play
+     * @param type the type of the play
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    /**
+     * Returns the play's name.
+     * @return the name of the play
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the type of the play.
+     * @return the type of the play
+     */
+    public String getType() {
+        return type;
     }
 }
